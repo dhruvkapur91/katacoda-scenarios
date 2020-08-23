23716fa51c389820585c96ead5a26a8a6618c083
Dhruv Kapur <dhruvkapur91@gmail.com>
2020-08-23 22:33:35 +0530

    Try. Running some script in the background

---
+++ docker-and-gradle/background.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+apt install -y openjdk-14-jdk
+
+java -version
+
+export JAVA_HOME=/usr/lib/jvm/java-1.14.0-openjdk-amd64/
+
+git clone https://github.com/dhruvkapur91/dockerize-gradle-springboot-app.git
