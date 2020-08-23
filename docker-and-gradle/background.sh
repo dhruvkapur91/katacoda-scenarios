469c7eab3661997da2bb238669652dc48efdb2d5
Dhruv Kapur <dhruvkapur91@gmail.com>
2020-08-23 23:15:46 +0530

    Fix. Be explicit about the background.log location, else changing cd commands change it

--- docker-and-gradle/background.sh
+++ docker-and-gradle/background.sh
@@ -1,15 +1,15 @@
-echo "Script Started" >> background.log
+echo "Script Started" >> /root/background.log
 
-apt install -y openjdk-14-jdk 2>&1 >> background.log
+apt install -y openjdk-14-jdk 2>&1 >> /root/background.log
 
-echo "Setting The Java Home" >> background.log
+echo "Setting The Java Home" >> /root/background.log
 export JAVA_HOME=/usr/lib/jvm/java-1.14.0-openjdk-amd64/
 
-echo "Cloing the repo" >> background.log 
+echo "Cloing the repo" >> /root/background.log 
 git clone https://github.com/dhruvkapur91/dockerize-gradle-springboot-app.git
 
-echo "Executing the spring boot project" >> background.log
+echo "Executing the spring boot project" >> /root/background.log
 cd dockerize-gradle-springboot-app
-./gradlew bootRun & 2>&1 > /dev/null
+./gradlew bootRun 2>&1 > /dev/null &
 
-echo "Please go and check at localhost:8080" >> background.log
+echo "Please go and check at localhost:8080" >> /root/background.log
