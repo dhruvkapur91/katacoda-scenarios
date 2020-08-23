0c37a862946af142058571f137676a4e9b500f0f
Dhruv Kapur <dhruvkapur91@gmail.com>
2020-08-23 22:55:01 +0530

    Add. running gradle app as background script

--- docker-and-gradle/background.sh
+++ docker-and-gradle/background.sh
@@ -2,6 +2,14 @@ echo "Script Started" >> background.log
 
 apt install -y openjdk-14-jdk 2>&1 >> background.log
 
-# export JAVA_HOME=/usr/lib/jvm/java-1.14.0-openjdk-amd64/
+echo "Setting The Java Home" >> background.log
+export JAVA_HOME=/usr/lib/jvm/java-1.14.0-openjdk-amd64/
 
-# git clone https://github.com/dhruvkapur91/dockerize-gradle-springboot-app.git
+echo "Cloing the repo" >> background.log 
+git clone https://github.com/dhruvkapur91/dockerize-gradle-springboot-app.git
+
+echo "Executing the spring boot project" >> background.log
+cd dockerize-gradle-springboot-app
+./gradlew bootRun & 2>&1 > /dev/null
+
+echo "Please go and check at localhost:8080" >> background.log
